865d37f2cf73c3e4109eb6fc8dcdabacfcb87472
NikitaFeodonit <nfeodonit@yandex.com>
2018-01-01 02:20:40 +0300

    Update license header to 2018 year.

--- cmake/FindAllegro.cmake
+++ cmake/FindAllegro.cmake
@@ -3,7 +3,7 @@
 #  Purpose:  A CMake build script for Allegro Library
 #  Author:   NikitaFeodonit, nfeodonit@yandex.com
 # ****************************************************************************
-#    Copyright (c) 2017 NikitaFeodonit
+#    Copyright (c) 2017-2018 NikitaFeodonit
 #
 #    This file is part of the LibCMaker_Allegro project.
 #
