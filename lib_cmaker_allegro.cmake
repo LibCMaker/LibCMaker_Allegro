44339f6df60c428079fc1254b7c241bb53e5a49a
NikitaFeodonit <nfeodonit@yandex.com>
2018-04-08 12:59:49 +0300

    Add a message printing on the build start.

--- lib_cmaker_allegro.cmake
+++ lib_cmaker_allegro.cmake
@@ -33,6 +33,7 @@ include(CMakeParseArguments) # cmake_parse_arguments
 
 include(cmr_lib_cmaker)
 include(cmr_print_debug_message)
+include(cmr_print_message)
 include(cmr_print_var_value)
 
 
@@ -44,6 +45,8 @@ list(APPEND CMAKE_MODULE_PATH "${lcm_LibCMaker_Allegro_SRC_DIR}/cmake/modules")
 
 function(lib_cmaker_allegro)
   cmake_minimum_required(VERSION 3.2)
+
+  cmr_print_message("======== Build library: Allegro ========")
 
   set(options
     # optional args
